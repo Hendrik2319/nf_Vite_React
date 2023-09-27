12815b8895939973bd0bc0a786db7f6642b8f11e
Hendrik Scholtz <135076120+Hendrik2319@users.noreply.github.com>
2023-09-27 17:00:15 +0200

    added Props to BlogEntry, added a callback function to Content & BlogEntry, added a useState to BlogEntry

--- src/BlogEntry.tsx
+++ src/BlogEntry.tsx
@@ -1,15 +1,27 @@
 import './BlogEntry.css'
+import {useState} from "react";
 
-export default function BlogEntry() {
+type BlogEntryProps = {
+    title:string
+    author:string
+    onclick: (message: string) => void
+}
+
+export default function BlogEntry( props: BlogEntryProps ) {
+    const [count, setCount] = useState(0)
+
     return (
         <div className="BlogEntry">
-            <h3>HTML</h3>
+            <h3>{props.title}</h3>
             <ul className="BlogEntryContent">
                 <li>Grundgerüst des Web-Frontends</li>
                 <li>besteht aus Tags: &lt;tag&gt;Text&lt;/tag&gt;</li>
             </ul>
-            <div className="Author">
-                Hendrik Scholtz
+            <div className="Author">{props.author}</div>
+            <div>Counter: {count}</div>
+            <div>
+                <button onClick={() => props.onclick("\""+props.title+"\" was clicked (counter: "+count+")")}>Click Me</button>
+                <button onClick={() => setCount((n)=>n+1)}>raise counter</button>
             </div>
         </div>
     )
--- src/Content.tsx
+++ src/Content.tsx
@@ -2,20 +2,34 @@ import './Content.css'
 import BlogEntry from './BlogEntry.tsx'
 
 export default function Content() {
+    //const defaultAuthor = "Hendrik Scholtz"
+    const data = [
+        { title:"Title", author:"Hendrik Scholtz" },
+        { title:"Title", author:"Hendrik Scholtz" },
+        { title:"Title", author:"Hendrik Scholtz" },
+        { title:"Title", author:"Hendrik Scholtz" },
+        { title:"Title", author:"Hendrik Scholtz" },
+        { title:"Title", author:"Hendrik Scholtz" },
+    ]
+
+    function buttonWasClicked( key:number, message:string ) {
+        alert( "Button was clicked: ["+ key +"] "+ message );
+    }
+
     return (
         <div className="Content">
             <div className="BlogList">
-                <BlogEntry/>
-                <BlogEntry/>
-                <BlogEntry/>
-                <BlogEntry/>
-                <BlogEntry/>
-                <BlogEntry/>
-                <BlogEntry/>
-                <BlogEntry/>
-                <BlogEntry/>
-                <BlogEntry/>
-                <BlogEntry/>
+                {
+                    data.map(
+                        (entry , index) =>
+                        <BlogEntry
+                            key={index}
+                            title={"["+index+"] "+entry.title}
+                            author={entry.author}
+                            onclick={message => buttonWasClicked(index, message)}
+                        />
+                    )
+                }
             </div>
         </div>
     )
